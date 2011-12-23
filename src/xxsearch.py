7fc66db3f6846e47d598eb3f92a59335af532593
Russ Ferriday <russf@topia.com>
2011-12-23 17:38:28 +0000

    ?

---
+++ src/xxsearch.py
@@ -0,0 +1,22 @@
+import time, random
+from xgoogle.search import GoogleSearch, SearchError
+
+f = open('a.txt','wb')
+
+for i in range(0,1000):
+    wt = random.uniform(3, 10)
+    q = '"manage.py" "settings.py" "urls.py" intitle:"- Github" -inurl:/wiki/ -inurl:/blob/ -inurl:manage.py -inurl:commits -inurl:README -inurl:gist site:github.com'
+    gs = GoogleSearch(q)
+    gs.results_per_page = 100
+    gs.page = i
+    results = gs.get_results()
+    #Try not to annnoy Google, with a random short wait
+    time.sleep(wt)
+    print 'This is the %dth iteration and waited %f seconds' % (i, wt)
+    for res in results:
+        f.write(res.url.encode("utf8"))
+        f.write("\n")
+    f.flush()
+
+print "Done"
+f.close()
